9e1dbba3b3035ecf05c107eb3f9576ba26bd7f93
Suleiman <prosperoil001@gmail.com>
2026-06-04 19:12:25 +0100

    Add Supabase project configuration

--- lib/src/core/config/supabase_config.dart
+++ lib/src/core/config/supabase_config.dart
@@ -2,6 +2,7 @@ class SupabaseConfig {
   const SupabaseConfig._();
 
   static const bool enabled = false;
-  static const String url = '';
-  static const String publishableKey = '';
+  static const String url = 'https://dfvvxytxsysvxvhjqgkx.supabase.co';
+  static const String publishableKey =
+      'sb_publishable_upsPItfO8cE9-kc-K9aCLA_1XTejT5V';
 }
